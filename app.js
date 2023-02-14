7fd6c00fe6efee9bbf8e4b57c0843ac1a9de3dbd
ergunDevv <ergncyln2127088@gmail.com>
2023-02-14 09:37:34 +0300

    fetch the api, get the data , show the user

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const app = Vue.createApp({
-    template : '<h1>Hello {{firstName}}</h1>',
+    
     data () {
         return {
-            firstName: 'John'
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+          }
+    },
+    methods:{
+        async getUser(){
+            const res =await fetch('https://randomuser.me/api');
+            const data = await res.json();
+            console.log(data.results[0])
+            console.log(this.firstName);
+            this.firstName = data.results[0].name.first
+            this.lastName = data.results[0].name.last
+            this.email = data.results[0].email
+            this.gender = data.results[0].gender
+            this.picture = data.results[0].picture.large
         }
     }
 
